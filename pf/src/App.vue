7470bf24348ad19fd726cfeb81c9e62b6b96e171
Theojkydbz <theo.geiller@gmail.com>
2022-02-16 09:35:19 -0500

    V1

---
+++ pf/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+   <div id="app">
+    <!--<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    document.getElementById("app").classList.add('show');
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Fraunces:wght@100;200;300;400;500;600;700;800;900&display=swap');
+html{
+  background-image: url("./assets/bg.png");
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  margin-top: 20vh;
+  
+}
+@keyframes example {
+  0%   {opacity: 0%;transform: scale(0.7)}
+  50%  {opacity: 0%;transform: scale(0.9)}
+  100% {opacity: 100%;transform: scale(1)}
+}
+#app.show{
+  display: block;
+  animation-name: example;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ pf/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <img class="lock" alt="Vue logo" src="../assets/lock.svg">
+    <img class="logo" alt="Vue logo" src="../assets/logo.svg">
+    <h2>product designer</h2>
+    <CodeLock/>
+    <p>private portfolio — reach me to see excerpt of my work</p>
+    <Social/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CodeLock from '@/components/CodeLock.vue'
+import Social from '@/components/Social.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    CodeLock,
+    Social
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.home{
+  display: flex;
+  flex-direction: column;
+}
+img{
+  margin: auto;
+  padding: 7px;
+}
+.lock{
+  width:35px;
+  margin-bottom: 15px;
+}
+.logo{
+  width:110px;
+}
+h2{
+margin-top: 2px;
+
+font-size: 1.8rem;
+font-family: Fraunces;
+font-style: 100;
+font-weight: 300;
+text-align: center;
+
+color: #E05151;
+}
+p{
+  font-family: Fraunces;
+  font-weight:200;
+}
+</style>
